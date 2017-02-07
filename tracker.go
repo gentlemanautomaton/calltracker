140cecb544fdea4f7ca6cf2c03270098cc6178a5
Joshua Sjoding <ga@josh.hey.nu>
2017-02-06 23:37:53 -0800

    Added subscription capability to tracker

--- tracker.go
+++ tracker.go
@@ -5,6 +5,15 @@ import (
 	"time"
 )
 
+// Subscriber is a function that receivers tracker event notifications.
+type Subscriber func(update Update)
+
+// Update is an update to the tracker's value
+type Update struct {
+	Sequence uint64
+	Value    Value
+}
+
 // TrackedCall is a call for which completion can be signaled.
 type TrackedCall struct {
 	Call
@@ -26,9 +35,11 @@ func (tc *TrackedCall) Done() {
 // It is safe to call its functions from more than one goroutine
 // simulataneously.
 type Tracker struct {
-	mutex sync.RWMutex
-	queue Queue
-	next  uint64 // The next ID to hand out.
+	mutex       sync.RWMutex
+	queue       Queue
+	next        uint64 // The next ID to hand out.
+	sequence    uint64 // The next update sequence number to hand out.
+	subscribers []Subscriber
 }
 
 // Add will add a call to the tracker. The call's start time will be the time
@@ -50,6 +61,7 @@ func (t *Tracker) Add() (call TrackedCall) {
 	}
 	t.queue.Push(call.Call)
 	t.next++
+	t.notify()
 	t.mutex.Unlock()
 
 	return
@@ -64,11 +76,44 @@ func (t *Tracker) Value() (value Value) {
 	return
 }
 
+// Subscribe will add the given subscriber function as a listener that will
+// receive updates whenever the tracker's value changes.
+func (t *Tracker) Subscribe(s Subscriber) {
+	t.mutex.Lock()
+	t.subscribers = append(t.subscribers, s)
+	t.mutex.Unlock()
+}
+
 func (t *Tracker) remove(number uint64) {
 	t.mutex.Lock()
 	i := t.queue.Index(number)
 	if i >= 0 {
 		t.queue.Remove(i)
+		t.notify()
 	}
 	t.mutex.Unlock()
+}
+
+// notify will send an event to the subscribers of the tracker informing them
+// of the value change.
+//
+// notify does not lock the tracker. It is the caller's responsibility to hold
+// a lock on the tracker for the duration of the call.
+func (t *Tracker) notify() {
+	if len(t.subscribers) == 0 {
+		return
+	}
+
+	subscribers := make([]Subscriber, len(t.subscribers))
+	copy(subscribers, t.subscribers)
+
+	var update Update
+	update.Value = make(Value, len(t.queue))
+	copy(update.Value, t.queue)
+
+	for _, s := range subscribers {
+		go s(update)
+	}
+
+	t.sequence++
 }
